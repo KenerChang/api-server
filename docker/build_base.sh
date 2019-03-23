f360ded0d3070a21467bcf6cb074451ba6a83c2a
kener's <workinkener@gmail.com>
2019-03-23 23:32:59 +0800

    add dockerfile for building base image

---
+++ docker/build_base.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Get docker image tags and export to environment variables
+set -a
+
+BUILDROOT="${DIR}/.."
+
+# Build docker image
+GIT_HEAD="$(git rev-parse --short HEAD)"
+IMAGE="api-base":$GIT_HEAD"
+
+docker build -t $IMAGE -f Dockerfile-base  $BUILDROOT
