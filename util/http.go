e938c34e18a7109d3ff3aeab746c50a26368aa60
kener's <workinkener@gmail.com>
2019-03-24 19:33:45 +0800

    http sugar function

---
+++ util/http.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// WriteJSON is a sugar function which handle response json
+func WriteJSON(w http.ResponseWriter, obj interface{}) error {
+	js, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Write(js)
+	return nil
+}
+
+// ReadJSON is a sugar function which decodes json obj from request
+func ReadJSON(r *http.Request, target interface{}) error {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	err := decoder.Decode(target)
+	return err
+}
